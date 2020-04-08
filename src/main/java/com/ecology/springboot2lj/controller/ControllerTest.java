b089051726d6b0cb17ccf1eec97491caf53cd586
luojie <1171156631@qq.com>
2020-04-08 18:46:36 +0800

    单独提交2

--- src/main/java/com/ecology/springboot2lj/controller/ControllerTest.java
+++ src/main/java/com/ecology/springboot2lj/controller/ControllerTest.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerTest {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hello1")
     public String test1(){
         return "helloGit";
     }
 
-    @RequestMapping("/hello1")
-    public String test11(){
-        return "helloGit";
-    }
-
     @RequestMapping("/hello2")
-    public String test12(){
+    public String test2(){
         return "helloGit";
     }
 
     @RequestMapping("/hello3")
-    public String test13(){
+    public String test3(){
+        return "helloGit";
+    }
+
+    @RequestMapping("/hello4")
+    public String test4(){
         return "helloGit";
     }
 }
