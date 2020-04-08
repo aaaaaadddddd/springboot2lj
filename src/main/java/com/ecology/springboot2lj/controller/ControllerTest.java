109652861f78fbe52fa8f76f0cc5ff1fb3db8a6b
luojie <1171156631@qq.com>
2020-04-08 18:18:53 +0800

    初次提交版本2

---
+++ src/main/java/com/ecology/springboot2lj/controller/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.ecology.springboot2lj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+    @RequestMapping("/hello")
+    public String test1(){
+        return "helloGit";
+    }
+}
---
+++ src/test/java/com/ecology/springboot2lj/Springboot2ljApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecology.springboot2lj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2ljApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
